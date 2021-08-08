5d095caf8c3d3c05bdd997156073e6d573a33058
ParthKataria <70621812+ParthKataria@users.noreply.github.com>
2021-08-08 19:34:08 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -12,3 +12,22 @@ void sort012(int a[], int n)
                 swap(a[md],a[h--]);
         }
     }
+// group array in neagtive and positive
+void rearrange(vector<int>&v)
+{
+	int l=0,h=v.size()-1;
+	while(l<=h)
+	{
+		if(v[l]<0 and v[h]<0)
+			l++;
+		else if(v[l]<0 and v[h]>0)
+		{
+		    l++;
+		    h--;
+		}
+		else if(v[l]>0 and v[h]<0)
+			swap(v[l],v[h--]);
+		else
+		    h--;
+	}
+}
